5e5c7b9ad193b4a7b566baa987bb4bae1a59fcb9
Enzo Ulloa <enzo.ulloa.i@gmail.com>
2025-10-18 15:19:15 -0300

    add clase 8 and assets

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import EducacionComponente from './components/EducacionComponente.vue';
 import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectosComponente from './components/ProyectosComponente.vue';
 </script>
 
 <template>
@@ -16,13 +17,18 @@ import ExperienciaComponente from './components/ExperienciaComponente.vue';
     <!-- el estilo css de section se encuentra en base.css -->
     <section id="educacion">
       <h2>Educacion - Cursos</h2>
+      <!-- Componente educacion -->
       <EducacionComponente />
     </section>
     <section id="experiencia">
       <h2>Experiencia</h2>
+      <!-- Componente experiencia -->
       <ExperienciaComponente />
     </section>
-    <section id="proyectos"><h2>Proyectos</h2></section>
+    <section id="proyectos">
+      <h2>Proyectos</h2>
+      <ProyectosComponente/>
+    </section>
     <section id="habilidades"><h2>Habilidades</h2></section>
     <section id="intereses"><h2>Intereses</h2></section>
   </main>
--- src/components/ExperienciaComponente.vue
+++ src/components/ExperienciaComponente.vue
@@ -1,19 +1,19 @@
 <script setup>
-// import icono1 from '/src/assets/ventas.svg';
-// import icono2 from '/src/assets/ecomerce.svg';
-// import icono3 from '/src/assets/configuracion.svg';
-// import icono4 from '/src/assets/uiux.svg';
-// import icono5 from '/src/assets/watch.svg';
+import icono1 from '/src/assets/ventas.svg';
+import icono2 from '/src/assets/ecomerce.svg';
+import icono3 from '/src/assets/configuracion.svg';
+import icono4 from '/src/assets/uiux.svg';
+import icono5 from '/src/assets/watch.svg';
 
 import { ref } from 'vue';
 const titulo = 'Desarrollador Full Stack';
 const fecha = 'Desde Enero 2022 / Julio 2024';
 const experiencias = ref([
-    { id: 1, parrafo: 'Lideré un equipo de 5 desarrolladores en la creación de una plataforma de comercio electrónico que incrementó las ventas en un 35% durante el primer año.' },
-    { id: 2, parrafo: 'Diseñé y desarrollé aplicaciones web utilizando Node.js, React y MongoDB, logrando reducir el tiempo de carga de las páginas en un 50%.' },
-    { id: 3 , parrafo: 'Implementé soluciones de autenticación y autorización, mejorando la seguridad del sitio web y reduciendo los intentos de acceso no autorizado en un 20%.' },
-    { id: 4,  parrafo: 'Colaboré con diseñadores de UX/UI para crear interfaces de usuario atractivas y fáciles de usar, aumentando la satisfacción del cliente en un 15%.' },
-    { id: 5, parrafo: 'Automaticé procesos de despliegue continuo utilizando Jenkins y Docker, reduciendo los tiempos de despliegue en un 40%.' },
+    { id: 1, src: icono1, parrafo: 'Lideré un equipo de 5 desarrolladores en la creación de una plataforma de comercio electrónico que incrementó las ventas en un 35% durante el primer año.' },
+    { id: 2, src: icono2, parrafo: 'Diseñé y desarrollé aplicaciones web utilizando Node.js, React y MongoDB, logrando reducir el tiempo de carga de las páginas en un 50%.' },
+    { id: 3, src: icono3, parrafo: 'Implementé soluciones de autenticación y autorización, mejorando la seguridad del sitio web y reduciendo los intentos de acceso no autorizado en un 20%.' },
+    { id: 4, src: icono4, parrafo: 'Colaboré con diseñadores de UX/UI para crear interfaces de usuario atractivas y fáciles de usar, aumentando la satisfacción del cliente en un 15%.' },
+    { id: 5, src: icono5, parrafo: 'Automaticé procesos de despliegue continuo utilizando Jenkins y Docker, reduciendo los tiempos de despliegue en un 40%.' },
 ]);
 </script>
 
@@ -23,7 +23,7 @@ const experiencias = ref([
         <p class="fecha">{{ fecha }}</p>
         <ul class="listado">
             <li class="item" v-for="experencia in experiencias" :key="experencia.id">
-                <!-- <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo"> -->
+                <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo">
                 <p>{{ experencia.parrafo }}</p>
             </li>
 
